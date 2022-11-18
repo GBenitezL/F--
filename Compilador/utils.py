4e9b9ca834474057f29ac518feb5ec19b8b6b011
Gustavo Benitez <benitezlgustavo@gmail.com>
2022-11-18 00:54:49 -0600

    Added Operation Codes

--- Compilador/utils.py
+++ Compilador/utils.py
@@ -1,37 +1,49 @@
 import sys
 
-data_types = {
-    'INTEGER': 'int',
-    'FLOAT': 'float',
-    'CHAR': 'char',
-    'BOOLEAN': 'bool',
-    'VOID': 'void',
-}
-
 data_type_IDs = {
-    data_types['INTEGER']: 1, 
-    data_types['FLOAT']: 2, 
-    data_types['CHAR']: 3, 
-    data_types['BOOLEAN']: 4, 
+    'int': 1, 
+    'float': 2, 
+    'char': 3, 
+    'bool': 4, 
 }
 
-logical_operators = {
-    'SUM': '+',
-    'MINUS': '-',
-    'DIVISION' : '/',
-    'TIMES' : '*',
-    'LESSTHAN' : '<',
-    'LESSEQUALTHAN' : '<=',
-    'GREATERTHAN' : '>',
-    'GREATEREQUALTHAN' : '>=',
-    'EQUAL' : '==',
-    'DIFFERENT' : '!=',
-    'AND' : '&&',
-    'OR' : '||',
-    'EQUALS' : '='
+
+operator_IDs = {
+    '+': 1,
+    '-': 2,
+    '/': 3,
+    '*': 4,
+    '<': 5,
+    '<=': 6,
+    '>': 7,
+    '>=': 8,
+    '==': 9,
+    '!=': 10,
+    '&&': 11,
+    '||': 12,
+    '=': 13,
+    
+    'GOTO': 20,
+    'GOTOV': 21,
+    'GOTOF': 22,
+    'GOSUB': 23,
+    'ERA': 24,
+    'VERIFY': 25,
+    'PARAM': 26,
+    'ENDFUNC': 27,
+    'END': 28,
+
+    'RETURN': 30,
+    'PRINT': 31,
+    'READ': 32,
+    'MEAN': 33,
+    'MEDIAN': 34,
+    'VARIANCE': 35,
+    'STD': 36,
+    'RAND': 37,
+    'PLOT': 38,
 }
 
 def print_error(message, id=''):
-    print(f'Error ID:{id}')
-    print(f'Description:', message, '\n')
+    print(f'Error ID:{id}\n', f'Description: {message}', '\n')
     sys.exit()
